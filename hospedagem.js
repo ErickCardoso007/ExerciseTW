ee828285ba78b2cee82d1286e6dfc2d158ebc9a0
Erick Cardoso <cyber666king@gmail.com>
2022-12-14 01:12:27 -0300

    feat(exerciseTW/hospedagem) cria função teste dentro da classe

--- hospedagem.js
+++ hospedagem.js
@@ -1,7 +1,22 @@
 class Hospedagem {
-    constructor(name) {
+    constructor(name, idade) {
         this.name = name
+        this.idade = idade
+    }
+
+    valida() {
+        if (this.idade < 18) {
+            return "Acesso negado, " + this.name + "!"
+        } else {
+            return "Você tem acesso a este conteúdo, " + this.name + "!"
+        }
     }
 }
 
-const hospedagem = new Hospedagem('Yuri')
+const parqueDasFlores = new Hospedagem('Gui', 29)
+const jardimBotanico = new Hospedagem('Will', 10)
+const marAtlantico = new Hospedagem('Leo', 22)
+
+console.log(parqueDasFlores.valida());
+console.log(jardimBotanico.valida());
+console.log(marAtlantico.valida());
